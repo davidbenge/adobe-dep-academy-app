02e9c0eb4129f72c50f8612a48be477698ace07b
David Benge <dbenge@adobe.com>
2022-12-22 17:53:11 -0800

    tweaking small issues to build base

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>This is the first Tab of the Adobe DEP Academy</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
